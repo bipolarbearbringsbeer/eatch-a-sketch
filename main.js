cc7e3cb709d4a502bb7092a97b33392bb87d8f14
bipolarbearbringsbeer <bipolarbearbringsbeer@gmail.com>
2024-06-15 17:12:36 +0300

    Add drawing functionality (changing color on mouse moving)

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-let gridSize = 16;
+// let gridSize = 16;
+// let container = document.querySelector(".container");
+// let row = document.createElement("div");
+// row.classList.add("row");
+
+// function createField() {
+//     for (let i = 0; i < gridSize; i++) {
+//         let square = document.createElement("div");
+//         square.classList.add("square");
+//         row.appendChild(square);
+//     }
+
+//     while (gridSize > 0) {
+//         container.appendChild(row.cloneNode(true));
+//         gridSize -= 1;
+//     }
+
+
+// }
+
+// createField();
+
+let gridSize = 100;
 let container = document.querySelector(".container");
-let row = document.createElement("div");
-row.classList.add("row");
 
 function createField() {
+    let fragment = document.createDocumentFragment();
+
     for (let i = 0; i < gridSize; i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+        let row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < gridSize; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+
+            square.addEventListener("mouseenter", () => {
+                square.classList.add("color");
+            })
+        }
+
+        fragment.appendChild(row);
     }
 
-    while (gridSize > 0) {
-        container.appendChild(row.cloneNode(true));
-        gridSize -= 1;
-    }
+    container.appendChild(fragment);
+
 }
 
 createField();
