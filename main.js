547e7f652ee89840687c6a6619b580ead034ff00
bipolarbearbringsbeer <bipolarbearbringsbeer@gmail.com>
2024-06-16 17:12:09 +0300

    Change style for drawing field

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
-// let gridSize = 16;
-// let container = document.querySelector(".container");
-// let row = document.createElement("div");
-// row.classList.add("row");
-
-// function createField() {
-//     for (let i = 0; i < gridSize; i++) {
-//         let square = document.createElement("div");
-//         square.classList.add("square");
-//         row.appendChild(square);
-//     }
-
-//     while (gridSize > 0) {
-//         container.appendChild(row.cloneNode(true));
-//         gridSize -= 1;
-//     }
-
-
-// }
-
-// createField();
-
-let gridSize = 100;
+let gridSize = 48;
 let container = document.querySelector(".container");
 
 function createField() {
